af22a30e63ef8e4be0f1a453f88233412faf09b0
khonager <khonager@gmail.com>
2026-09-17 11:32:38 +0200

    fix github build

--- app/src/test/kotlin/dev/tipstroke/app/BrushPreferencesTest.kt
+++ app/src/test/kotlin/dev/tipstroke/app/BrushPreferencesTest.kt
@@ -8,8 +8,10 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.RuntimeEnvironment
+import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
+@Config(sdk = [35])
 class BrushPreferencesTest {
     @Test fun tuningPersistsPerBrushAndSeparatelyForEraser() {
         val context = RuntimeEnvironment.getApplication()
--- app/src/test/kotlin/dev/tipstroke/app/MainActivityConfigurationTest.kt
+++ app/src/test/kotlin/dev/tipstroke/app/MainActivityConfigurationTest.kt
@@ -6,8 +6,10 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
+@Config(sdk = [35])
 class MainActivityConfigurationTest {
     @Test fun orientationAndScreenLayoutChangesKeepTheActiveEditorActivity() {
         val activity = Robolectric.buildActivity(MainActivity::class.java).setup().get()
